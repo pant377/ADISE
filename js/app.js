6e044d2a543065753e1475b39d4d258ef797999d
gsak3l <gsakel5@gmail.com>
2019-12-11 16:55:57 +0200

    text for the onclick added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function startRound() {
+    document.getElementById('centerDiv').innerHTML = 'card on the deck<h3>R6</h3>';
+}
+function changeColor(cellx) {
+    console.log('i amhere');
+    cellx.style.backgroundColor= "red";
+}
