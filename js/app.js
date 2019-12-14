4bed223ecc7ae217860b0ef11e7e9c961e94646a
pant377 <pantpap15@gmail.com>
2019-12-14 15:54:46 +0200

    sdf

--- js/app.js
+++
@@ -1,3 +0,0 @@
-function startRound() {
-    document.getElementById('centerDiv').innerHTML = 'card on the deck<h3>R6</h3>';
-}
---
+++ uno.php
@@ -0,0 +1,47 @@
+<?php require_once "lib/dbconnect.php";
+require_once "lib/board.php";
+require_once "lib/game.php";
+$method=$_SERVER['REQUEST_METHOD'];
+$request=explode('/', trim($_SERVER['PATH_INFO'], '/'));
+$input=json_decode(file_get_contents('php://input'), true);
+
+switch ($r=array_shift($request)) {
+        case 'game' : switch ($b=array_shift($request)) {
+                case '':
+                        case null: handle_game($method);
+                break;
+                case 'jola': echo ('jola');
+                break;
+                default: header("HTTP/1.1 404 Not Found");
+                break;
+        }
+
+        break;
+
+        case 'status': if(sizeof($request)==0) {
+                show_status();
+        }
+
+        else {
+                header("HTTP/1.1 404 Not Found");
+        }
+
+        break;
+        case 'players': echo ("hija");
+        break;
+        default: header("HTTP/1.1 404 Not Found");
+        exit;
+}
+
+function handle_game($method) {
+
+        if($method=='GET') {
+                show_game();
+        }
+
+        else if ($method=='POST') {
+                reset_game();
+        }
+
+}
+?>
