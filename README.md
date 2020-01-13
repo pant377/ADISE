6f369bf964b16289ba098cf6f93ea1a3db2e163f
pant377 <pantpap15@gmail.com>
2020-01-13 16:18:57 +0200

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# ADISE19_GSPP
--- lib/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$mysqli = mysqli_connect("localhost", "root", "", "uno");	
-	$sql = 'INSERT INTO hand (playerId, cardId) VALUES (1, 3)';
-	$st = $mysqli->prepare($sql);
-	$st->execute();
-?>
